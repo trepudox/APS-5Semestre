602d79c92c9924760b37fe5ad261efe05ed39c94
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-04-27 00:31:25 -0300

    Rascunho funcional de comunicacao assincrona

--- TCP-Client/src/main/java/com/trepudox/Client.java
+++ TCP-Client/src/main/java/com/trepudox/Client.java
@@ -1,47 +1,52 @@
 package com.trepudox;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.InetAddress;
-import java.net.Socket;
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
 import java.util.Scanner;
+import java.util.concurrent.Future;
 
 public class Client {
+    // 1024 Bytes corresponde a uma mensagem de aproximadamente 1000 caracteres
 
-    private static final Scanner scanner = new Scanner(System.in);
+    // Caso usar uma máquina diferente para o servidor, consultar o IP do mesmo e alterar essa variável
+    private static final String ADDRESS = "127.0.0.1";
+    private static final int PORT = 10000;
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        try {
-
+        try(AsynchronousSocketChannel client = AsynchronousSocketChannel.open()) {
             String identificacao = "";
             String mensagem = "";
 
-            while(!identificacao.equals("0") && !mensagem.equals("0")) {
-                // cria a conexao
-                Socket cliente = new Socket(InetAddress.getLocalHost(), 10000);
+            client.connect(new InetSocketAddress(InetAddress.getLocalHost(), PORT));
 
-                // objeto que cria a mensagem para outro host
-                ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-                saida.flush();
+            while(!identificacao.equals("0") && !mensagem.equals("0")) {
 
                 System.out.print("Digite sua identificação: ");
-                identificacao = scanner.nextLine();
+                identificacao = SCANNER.nextLine();
 
                 System.out.print("Digite sua mensagem: ");
-                mensagem = scanner.nextLine();
+                mensagem = SCANNER.nextLine();
 
-                LocalDateTime l = LocalDateTime.now();
-                System.out.println(l);
-                saida.writeObject(String.format("%s '%s': %s", LocalDateTime.now(), identificacao, mensagem));
+                String message = String.format("'%s': %s", identificacao, mensagem);
+                ByteBuffer writeBuffer = ByteBuffer.wrap(message.getBytes());
+                client.write(writeBuffer);
 
-                // objeto que recebe a mensagem do outro host
-                ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-                String retorno = entrada.readObject().toString();
-                System.out.println("Retorno: " + retorno);
+                writeBuffer.clear();
 
-                cliente.close();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                Future<Integer> readValue = client.read(readBuffer);
+
+                if(readValue.get() == -1) {
+                    System.out.println("Não foi possível ler a resposta do servidor.");
+                    break;
+                }
+
+                System.out.println("Retorno: " + new String(readBuffer.array()).trim());
+
+                readBuffer.clear();
                 System.out.println();
             }
         } catch(Exception e) {
--- TCP-Server/src/main/java/com/trepudox/Server.java
+++ TCP-Server/src/main/java/com/trepudox/Server.java
@@ -1,31 +1,57 @@
 package com.trepudox;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Date;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
 
     private static final int PORT = 10000;
+    private static final String DATE_TIME_PATTERN = "dd-MM-yyyy HH:mm:ss.SSSSSS";
 
     public static void main(String[] args) {
-        try(ServerSocket servidor = new ServerSocket(PORT)) {
+        try(AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()) {
+            server.bind(new InetSocketAddress(InetAddress.getLocalHost(), PORT));
             System.out.printf("Servidor ouvindo a porta %d%n", PORT);
 
-            while(!servidor.isClosed()) {
-                Socket cliente = servidor.accept();
-                System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
-                ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+            while(server.isOpen()) {
+                Future<AsynchronousSocketChannel> connection = server.accept();
+                AsynchronousSocketChannel client = connection.get(10, TimeUnit.SECONDS);
 
-                System.out.println("Mensagem: " + entrada.readObject());
-                ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-                saida.flush();
-                saida.writeObject("OK");
+                System.out.println("Cliente conectado: " + client.getRemoteAddress());
+                while(client.isOpen()) {
 
-                saida.close();
-                cliente.close();
+                    ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                    Future<Integer> readValue = client.read(readBuffer);
+
+                    if(readValue.get() == -1) {
+                        String returnMsg = "A mensagem enviada não pôde ser lida!";
+                        System.out.println(returnMsg);
+                        client.write(ByteBuffer.wrap(readBuffer.array()));
+                        client.close();
+                        break;
+                    }
+
+                    readBuffer.flip();
+                    String receivedMessage = new String(readBuffer.array()).trim();
+                    readBuffer.clear();
+
+                    LocalDateTime receivedMessageDateTime = LocalDateTime.now();
+                    String formattedDateTime = receivedMessageDateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
+                    System.out.printf("[%s] - %s%n", formattedDateTime, receivedMessage);
+
+                    String returnMsg = String.format("Mensagem recebida com sucesso na data e hora %s", formattedDateTime);
+                    ByteBuffer writeBuffer = ByteBuffer.wrap(returnMsg.getBytes());
+                    client.write(writeBuffer);
+                    writeBuffer.clear();
+                }
+
                 System.out.println();
             }
         } catch(Exception e) {
